184337fad814d1261f8fd0a13f3844cc78698820
Alex <alexf137@posteo.de>
2025-04-14 18:50:20 +0200

    Added handler for clicks on list items

--- js/jsl.js
+++ js/jsl.js
@@ -2,6 +2,7 @@ window.onload = function() {
   const mainContainer = document.getElementById("main-container");
   const mainView = document.querySelector("main");
   const viewStyleButton = document.getElementById("view-type-icon");
+  const listItems = document.getElementsByClassName("main-list-item");
   let currentViewStyle = "list-view";
 
   function toggleCurrentViewStyle() {
@@ -18,5 +19,24 @@ window.onload = function() {
       mainView.style.opacity = "100";
     }, 2000);
   }
+
+  function handleListItemClick(e, listItem) {
+    const itemTitle = listItem.getElementsByClassName("main-list-item-img-title")[0].textContent;
+    const imgUrl = listItem.getElementsByClassName("main-list-item-img-src")[0].textContent;
+    if (e.target.classList.contains("options-icon")) {
+      alert(`
+        Title: ${itemTitle}
+        URL: ${imgUrl}
+      `);
+    } else {
+      alert(itemTitle);
+    }
+  }
+
   viewStyleButton.addEventListener("click", toggleCurrentViewStyle);
+  for (let i = 0; i < listItems.length; i++) {
+    listItems.item(i).addEventListener("click", (e) => {
+      handleListItemClick(e, listItems.item(i));
+    });
+  }
 };
