cd6011db9f9747bcab56ccff14378a6d49a7bb31
Alex <alexf137@posteo.de>
2025-04-14 18:16:48 +0200

    Added fade animation for switch between view styles

--- js/jsl.js
+++ js/jsl.js
@@ -1,17 +1,22 @@
 window.onload = function() {
   const mainContainer = document.getElementById("main-container");
+  const mainView = document.querySelector("main");
   const viewStyleButton = document.getElementById("view-type-icon");
   let currentViewStyle = "list-view";
 
   function toggleCurrentViewStyle() {
-    mainContainer.classList.remove(currentViewStyle);
-    if (currentViewStyle === "list-view") {
-      mainContainer.classList.add("tile-view");
-      currentViewStyle = "tile-view";
-    } else {
-      mainContainer.classList.add("list-view");
-      currentViewStyle = "list-view";
-    }
+    mainView.style.opacity = "0";
+    setTimeout(() => {
+      mainContainer.classList.remove(currentViewStyle);
+      if (currentViewStyle === "list-view") {
+        mainContainer.classList.add("tile-view");
+        currentViewStyle = "tile-view";
+      } else {
+        mainContainer.classList.add("list-view");
+        currentViewStyle = "list-view";
+      }
+      mainView.style.opacity = "100";
+    }, 2000);
   }
   viewStyleButton.addEventListener("click", toggleCurrentViewStyle);
 };
