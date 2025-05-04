4d4b3dda869d0a19227a6829fb0940553419b143
Alex <alexf137@posteo.de>
2025-05-04 14:16:56 +0200

    Moved scripts to end of body element

--- js/jsl.js
+++ js/jsl.js
@@ -1,43 +1,40 @@
-window.onload = function() {
-  const mainContainer = document.getElementById("main-container");
-  const mainView = document.querySelector("main");
-  const viewStyleButton = document.getElementById("view-type-icon");
-  const listItems = document.getElementsByClassName("main-list-item");
-  let currentViewStyle = "list-view";
+const mainContainer = document.getElementById("main-container");
+const mainView = document.querySelector("main");
+const viewStyleButton = document.getElementById("view-type-icon");
+const listItems = document.getElementsByClassName("main-list-item");
+let currentViewStyle = "list-view";
 
-  function toggleCurrentViewStyle() {
-    console.log("Switching view style...");
-    mainView.style.opacity = "0";
-    setTimeout(() => {
-      mainContainer.classList.remove(currentViewStyle);
-      if (currentViewStyle === "list-view") {
-        mainContainer.classList.add("tile-view");
-        currentViewStyle = "tile-view";
-      } else {
-        mainContainer.classList.add("list-view");
-        currentViewStyle = "list-view";
-      }
-      mainView.style.opacity = "100";
-    }, 2000);
-  }
-
-  function handleListItemClick(e, listItem) {
-    const itemTitle = listItem.getElementsByClassName("main-list-item-img-title")[0].textContent;
-    const imgUrl = listItem.getElementsByClassName("main-list-item-img-src")[0].textContent;
-    if (e.target.classList.contains("options-icon")) {
-      alert(`
-        Title: ${itemTitle}
-        URL: ${imgUrl}
-      `);
+function toggleCurrentViewStyle() {
+  console.log("Switching view style...");
+  mainView.style.opacity = "0";
+  setTimeout(() => {
+    mainContainer.classList.remove(currentViewStyle);
+    if (currentViewStyle === "list-view") {
+      mainContainer.classList.add("tile-view");
+      currentViewStyle = "tile-view";
     } else {
-      alert(itemTitle);
+      mainContainer.classList.add("list-view");
+      currentViewStyle = "list-view";
     }
-  }
+    mainView.style.opacity = "100";}, 2000);
+}
 
-  viewStyleButton.addEventListener("click", toggleCurrentViewStyle);
-  for (let i = 0; i < listItems.length; i++) {
-    listItems.item(i).addEventListener("click", (e) => {
-      handleListItemClick(e, listItems.item(i));
-    });
+function handleListItemClick(e, listItem) {
+  const itemTitle = listItem.getElementsByClassName("main-list-item-img-title")[0].textContent;
+  const imgUrl = listItem.getElementsByClassName("main-list-item-img-src")[0].textContent;
+  if (e.target.classList.contains("options-icon")) {
+    alert(`
+       Title: ${itemTitle}
+       URL: ${imgUrl}
+     `);
+  } else {
+    alert(itemTitle);
   }
-};
+}
+
+viewStyleButton.addEventListener("click", toggleCurrentViewStyle);
+for (let i = 0; i < listItems.length; i++) {
+  listItems.item(i).addEventListener("click", (e) => {
+    handleListItemClick(e, listItems.item(i));
+  });
+}
--- js/jsr.js
+++ js/jsr.js
@@ -1,100 +1,86 @@
-window.onload = function () {
+const itemsList = document.getElementsByTagName("ul")[0];
+const refreshIcon = document.getElementById("refresh-icon");
 
-  const itemsList = document.getElementsByTagName("ul")[0];
-  const refreshIcon = document.getElementById("refresh-icon");
-
-  async function fetchListItems() {
-    const url = "http://192.168.178.20:8395/data/listitems.json";
-
-    try {
-      console.log("Fetching list items...");
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error(`List items could not be loaded: ${response.status}`);
-      }
-
-      return await response.json();
-    } catch (e) {
-      console.error(e.message);
+async function fetchListItems() {
+  const url = "http://192.168.178.20:8395/data/listitems.json";
+  try {
+    console.log("Fetching list items...");
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`List items could not be loaded: ${response.status}`);
     }
+    return await response.json();
+  } catch (e) {
+    console.error(e.message);
   }
+}
 
-  function createListItem(listItemObject) {
-    const newItem = document.createElement("li");
-    const imgContainer = document.createElement("div");
-    const containerCenter = document.createElement("div");
-    const containerRight = document.createElement("div");
-    const img = document.createElement("img");
-    const imgSrc = document.createElement("p");
-    const imgTitle = document.createElement("p");
-    const availableTagsContainer = document.createElement("div");
-    const tagsIcon = document.createElement("img");
-    const tagsCount = document.createElement("p");
-    const date = document.createElement("p");
-    const optionsIcon = document.createElement("button");
+function createListItem(listItemObject) {
+  const newItem = document.createElement("li");
+  const imgContainer = document.createElement("div");
+  const containerCenter = document.createElement("div");
+  const containerRight = document.createElement("div");
+  const img = document.createElement("img");
+  const imgSrc = document.createElement("p");
+  const imgTitle = document.createElement("p");
+  const availableTagsContainer = document.createElement("div");
+  const tagsIcon = document.createElement("img");
+  const tagsCount = document.createElement("p");
+  const date = document.createElement("p");
+  const optionsIcon = document.createElement("button");
+  // Assign inner container classes
+  imgContainer.className = "main-list-item-inner-container";
+  containerCenter.className = "main-list-item-inner-container";
+  containerRight.className = "main-list-item-inner-container main-list-item-inner-container-right"
+  availableTagsContainer.className = "available-tags-container"
+  // Build item image
+  img.src = listItemObject.src;
+  img.width = 125;
+  img.height = 125;
+  img.alt = "A random image";
+  img.className = "main-list-item-img";
+  imgContainer.appendChild(img);
+  // Prepare available tags container
+  tagsIcon.src = "./css/img/png/tag_FILL0_wght400_GRAD0_opsz24.png";
+  tagsIcon.width = 20;
+  tagsIcon.height = 20;
+  tagsIcon.alt = "Available tags arrow icon";
+  tagsIcon.className = "icon";
+  tagsCount.textContent = listItemObject.numOfTags;
+  tagsCount.className = "available-tags-count";
+  availableTagsContainer.append(tagsIcon, tagsCount);
+  // Build item inner container center
+  imgSrc.textContent = listItemObject.owner;
+  imgSrc.className = "main-list-item-img-src";
+  imgTitle.textContent = listItemObject.title;
+  imgTitle.className = "main-list-item-img-title";
+  containerCenter.append(imgSrc, imgTitle, availableTagsContainer);
+  // Build item inner container right
+  date.textContent = listItemObject.added;
+  date.className = "main-list-item-date";
+  optionsIcon.className = "standard-button icon options-icon";
+  containerRight.append(date, optionsIcon);
+  // Build list item
+  newItem.className = "main-list-item";
+  newItem.append(imgContainer, containerCenter, containerRight);
+  return newItem;
+}
 
-    // Assign inner container classes
-    imgContainer.className = "main-list-item-inner-container";
-    containerCenter.className = "main-list-item-inner-container";
-    containerRight.className = "main-list-item-inner-container main-list-item-inner-container-right"
-    availableTagsContainer.className = "available-tags-container"
+async function renderItemList() {
+  const listItemData = await fetchListItems();
+  listItemData.forEach((item) => {
+    const newListItem = createListItem(item);
+    itemsList.appendChild(newListItem);
+  });
+}
 
-    // Build item image
-    img.src = listItemObject.src;
-    img.width = 125;
-    img.height = 125;
-    img.alt = "A random image";
-    img.className = "main-list-item-img";
-    imgContainer.appendChild(img);
-
-    // Prepare available tags container
-    tagsIcon.src = "./css/img/png/tag_FILL0_wght400_GRAD0_opsz24.png";
-    tagsIcon.width = 20;
-    tagsIcon.height = 20;
-    tagsIcon.alt = "Available tags arrow icon";
-    tagsIcon.className = "icon";
-    tagsCount.textContent = listItemObject.numOfTags;
-    tagsCount.className = "available-tags-count";
-    availableTagsContainer.append(tagsIcon, tagsCount);
-
-    // Build item inner container center
-    imgSrc.textContent = listItemObject.owner;
-    imgSrc.className = "main-list-item-img-src";
-    imgTitle.textContent = listItemObject.title;
-    imgTitle.className = "main-list-item-img-title";
-    containerCenter.append(imgSrc, imgTitle, availableTagsContainer);
-
-    // Build item inner container right
-    date.textContent = listItemObject.added;
-    date.className = "main-list-item-date";
-    optionsIcon.className = "standard-button icon options-icon";
-    containerRight.append(date, optionsIcon);
-
-    // Build list item
-    newItem.className = "main-list-item";
-    newItem.append(imgContainer, containerCenter, containerRight);
-
-    return newItem;
-  }
-
-  async function renderItemList() {
-    const listItemData = await fetchListItems();
-
-    listItemData.forEach((item) => {
-      const newListItem = createListItem(item);
-      itemsList.appendChild(newListItem);
-    });
-  }
-
-  function refreshItemsList() {
-    itemsList.replaceChildren();
-    renderItemList();
-  }
-
-  // Register event listeners
-  refreshIcon.addEventListener("click", refreshItemsList);
-
-  // Main
+function refreshItemsList() {
+  itemsList.replaceChildren();
   renderItemList();
 }
+
+// Register event listeners
+refreshIcon.addEventListener("click", refreshItemsList);
+
+// Main
+renderItemList();
